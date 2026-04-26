3fdc98e3598768aa10f859ab7b140ee08db50ca1
xujinzhuo <jzxu@gkht.com>
2026-04-26 22:35:30 +0800

    Initial Commit

---
+++ pomodoro/pomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomodoroApp.swift
+//  pomodoro
+//
+//  Created by admin on 2026/4/26.
+//
+
+import SwiftUI
+
+@main
+struct pomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pomodoroTests/pomodoroTests.swift
@@ -0,0 +1,17 @@
+//
+//  pomodoroTests.swift
+//  pomodoroTests
+//
+//  Created by admin on 2026/4/26.
+//
+
+import Testing
+@testable import pomodoro
+
+struct pomodoroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
